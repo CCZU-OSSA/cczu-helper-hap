72b4eba61bb5321d223011d1140437cc0acc2403
liluolinder <136098524+liluolinder@users.noreply.github.com>
2025-02-27 18:48:07 +0800

    update 1

--- lib/messages/all.dart
+++
@@ -1,7 +0,0 @@
-export './generated.dart';
-export './account.pb.dart';
-export './cmcc.pb.dart';
-export './grades.pb.dart';
-export './icalendar.pb.dart';
-export './lab.pb.dart';
-export './update.pb.dart';
